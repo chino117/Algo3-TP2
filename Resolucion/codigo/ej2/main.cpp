ec689c3cab4fd2723f806d3920e6de265846a1e1
Emiliano <eslucero2010@gmail.com>
2019-05-11 11:52:13 -0300

    Puse para leer datos de entrada en ambos ejercicios

--- Resolucion/codigo/ej2/main.cpp
+++ Resolucion/codigo/ej2/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
 
+using namespace std;
+
+typedef vector<vector<int>> Matriz;
 
 // Emi: Este es el dijkstra que hicimos en el taller, lo pongo aca de ejemplo
 /*void dijkstra(int n, int s, const function<double(int i, int j)>& peso, vector<double>& distancias)
@@ -28,5 +33,28 @@
 */
 
 int main(int argc, char** argv){
+    if(argc < 4)
+        cout<<"ERROR: Faltan argumentos"<<endl;
+
+    // Leemos datos de entrada
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+
+    int i = 3;
+    vector<int> costoXciudad(n, -1);
+    for(;i < n+3; i++)
+        costoXciudad[i-3] = atoi(argv[i]);
+
+    Matriz litrosXeje(n, vector<int>(n, 0));
+    for(;i < 3+n+(3*m);i+=3){
+        int a_i = atoi(argv[i]);
+        int b_i = atoi(argv[i+1]);
+        int l_i = atoi(argv[i+2]);
+
+        litrosXeje[a_i-1][b_i-1] = l_i;
+        litrosXeje[b_i-1][a_i-1] = l_i;
+    }
+    // Terminamos de leer
+
     return 0;
 }
