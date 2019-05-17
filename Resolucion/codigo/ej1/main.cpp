32d1cd2d10bceb55e3bda6622922a5c9e7c50577
Emiliano <eslucero2010@gmail.com>
2019-05-17 14:09:58 -0300

    Segui un poco mas

--- Resolucion/codigo/ej1/main.cpp
+++ Resolucion/codigo/ej1/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <tuple>
 #include <algorithm>
+#include <memory>
 #include "disjoint_set.hpp"
 
 using namespace std;
@@ -21,6 +22,7 @@ void mostrar_matriz(const matriz& m){
         cout<<endl;
     }
 }
+
 
 // Como es un grafo, no un digrafo, podemos asumir en cada paso que ciertas aristas ya fueron agregadas
 vector<eje> crear_aristas_vecinos(int i, int j, const matriz& m){
@@ -42,6 +44,65 @@ vector<eje> crear_aristas_vecinos(int i, int j, const matriz& m){
     return res;
 }
 
+unsigned int internal_diff(unsigned int i, unsigned int j){
+    // TODO: Completar esto segun lo que dice el paper
+    // Hace falta pasarle el disjoint set
+    return 0;
+}
+
+// Esto vendria a ser el ciclo del algoritmo
+vector<eje> segmentar_aux(vector<eje>& E, shared_ptr<disjoint_set> U){
+    vector<eje> T;
+
+    for(int i = 0;i < E.size();i++){
+        auto o_q = E[i];
+        auto componente_v = U->find(v(o_q));
+        auto componente_w = U->find(w(o_q));
+        if(componente_v != componente_w && w(o_q) <= internal_diff(componente_v, componente_w))
+
+            U->unite(componente_v, componente_w);
+
+/* TODO: * Definir internal_diff */
+/*       * Que guardamos en caso de merge? */
+/*       * Que devolvemos? */
+
+        }
+    }
+
+    return T;
+}
+
+vector<eje> segmentar(const Matriz& m){
+    unsigned int n = unsigned int(m.size() * m[0].size());
+
+    // Armamos lista de aristas
+    vector<eje> E;
+    for(int i = 0;i < alto; i++){
+        for(int j = 0;j < ancho;j++){
+            vector<eje> vecinos(crear_aristas_vecinos(i, j, m));
+            for(auto & k : vecinos)
+                E.push_back(k);
+        }
+    }
+
+    // Ordenamos lista de aristas por peso
+    sort(E.begin(), E.end());
+    
+    /* Mostramos lista de aristas */
+    /* for(auto& i : E) */
+    /*     cout<<"("<<v(i)<<", "<<u(i)<<", "<<w(i)<<") "; */
+    /* cout<<endl; */
+
+    // Creamos el disjoint_set aca para que el algoritmo sea igual sin importar la implementacion
+    shared_ptr<disjoint_set> U = new disjoint_set_arreglo();
+    //shared_ptr<disjoint_set> U = new disjoint_set_arbol();
+    //shared_ptr<disjoint_set> U = new disjoint_set_arbol_optimizado();
+
+    U->create(n);// alto * ancho
+
+    return segmentar_aux(E, U);
+}
+
 int main(int argc, char** argv){
 
     if (argc < 3){
@@ -61,24 +122,8 @@ int main(int argc, char** argv){
     /* mostrar_matriz(m); */
 
     // Terminamos de leer
+    segmentar(m);
 
-    // Armamos lista de aristas
-    vector<eje> E;
-    for(int i = 0;i < alto; i++){
-        for(int j = 0;j < ancho;j++){
-            vector<eje> vecinos(crear_aristas_vecinos(i, j, m));
-            for(auto & k : vecinos)
-                E.push_back(k);
-        }
-    }
-
-    // Ordenamos lista de aristas por peso
-    sort(E.begin(), E.end());
-    
-    /* Mostramos lista de aristas */
-    /* for(auto& i : E) */
-    /*     cout<<"("<<v(i)<<", "<<u(i)<<", "<<w(i)<<") "; */
-    /* cout<<endl; */
-    
+        
     return 0;
 }
