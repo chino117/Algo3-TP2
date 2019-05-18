54795f0958c1a2e09213d267cf97ed193548cc99
Emiliano <eslucero2010@gmail.com>
2019-05-18 11:24:55 -0300

    Primera implementacion de disjoint_set sobre arreglo y segui lo del algoritmo

--- Resolucion/codigo/ej1/disjoint_set.hpp
+++ Resolucion/codigo/ej1/disjoint_set.hpp
@@ -1,21 +1,49 @@
 #ifndef DISJOINT_SET_HPP
 #define DISJOINT_SET_HPP
 
+#include <vector>
+
+using namespace std;
+
+// Unite(i, j): une ambas componentes bajo el identificador i
+
 class disjoint_set{
     public:
         virtual ~disjoint_set(){};
         virtual void create(unsigned int n) = 0;
         virtual void unite(unsigned int i, unsigned int j) = 0;
         virtual unsigned int find(unsigned int i) = 0;
+        virtual unsigned int size(unsigned int c) = 0;
 };
 
 class disjoint_set_arreglo : public disjoint_set{
     public:
-        virtual void create(unsigned int n);
-        virtual void unite(unsigned int i, unsigned int j);
-        virtual unsigned int find(unsigned int i);
+        virtual void create(unsigned int n){
+            componentes.resize(n);
+            for(unsigned int i = 0;i < n;i++)
+                componentes[i] = i;
+
+            tam_comps.resize(n, 1);
+        }
+        virtual void unite(unsigned int i, unsigned int j){
+            unsigned int comp_j = componentes[j];
+            unsigned int comp_i = componentes[i];
+            for(unsigned int k = 0;k < componentes.size();k++){
+                if(componentes[k] == comp_j){
+                    componentes[k] = comp_i;
+                    tam_comps[comp_i]++;
+                }
+            }
+        }
+        virtual unsigned int find(unsigned int i){
+            return componentes[i];
+        }
+        virtual unsigned int size(unsigned int c){
+            return tam_comps[c];
+        }
     private:
-        // Completar con la representacion
+        vector<unsigned int> componentes;
+        vector<unsigned int> tam_comps;
 };
 
 class disjoint_set_arbol : public disjoint_set{
@@ -23,6 +51,7 @@ class disjoint_set_arbol : public disjoint_set{
         virtual void create(unsigned int n);
         virtual void unite(unsigned int i, unsigned int j);
         virtual unsigned int find(unsigned int i);
+        virtual unsigned int size(unsigned int c);
     private:
         // Completar con la representacion
 };
@@ -32,6 +61,7 @@ class disjoint_set_arbol_optimizado : public disjoint_set{
         virtual void create(unsigned int n);
         virtual void unite(unsigned int i, unsigned int j);
         virtual unsigned int find(unsigned int i);
+        virtual unsigned int size(unsigned int c);
     private:
         // Completar con la representacion
 };
--- Resolucion/codigo/ej1/main.cpp
+++ Resolucion/codigo/ej1/main.cpp
@@ -4,6 +4,7 @@
 #include <tuple>
 #include <algorithm>
 #include <memory>
+#include <limits>
 #include "disjoint_set.hpp"
 
 using namespace std;
@@ -44,37 +45,45 @@ vector<eje> crear_aristas_vecinos(int i, int j, const matriz& m){
     return res;
 }
 
-unsigned int internal_diff(unsigned int i, unsigned int j){
-    // TODO: Completar esto segun lo que dice el paper
-    // Hace falta pasarle el disjoint set
-    return 0;
+unsigned int internal_diff(unsigned int i, unsigned int j, const vector<unsigned int>& w_c, unsigned int t_i, unsigned int t_j){
+    return min(w_c[i] + t_i, w_c[j] + t_j);
 }
 
 // Esto vendria a ser el ciclo del algoritmo
-vector<eje> segmentar_aux(vector<eje>& E, shared_ptr<disjoint_set> U){
+// Por cada componente necesitamos (ademas de lo provisto por disjoint_set) la
+// cantidad de elementos dentro de la componente(agregado a disjoint_set) y la
+// arista de peso maximo contenida en la componente (eso se mantiene aca)
+vector<eje> segmentar_aux(vector<eje>& E, shared_ptr<disjoint_set> U, int n, int k){
     vector<eje> T;
+
+    vector<unsigned int> w_max_x_comp(n, 0);
 
     for(unsigned int i = 0;i < E.size();i++){
         auto o_q = E[i];
         auto componente_v = U->find(v(o_q));
-        auto componente_w = U->find(w(o_q));
-        if(componente_v != componente_w && w(o_q) <= internal_diff(componente_v, componente_w))
+        auto componente_u = U->find(u(o_q));
 
-            U->unite(componente_v, componente_w);
+        // No me queda claro si la division deberia ser entre enteros o 
+        // hay que castear a float, hacer la division y castearla a entero
+        auto t_v = k/U->size(componente_v);
+        auto t_u = k/U->size(componente_u);
 
-/* TODO: * Definir internal_diff */
-/*       * Que guardamos en caso de merge? */
-/*       * Que devolvemos? */
+        if(componente_v != componente_u && w(o_q) <= internal_diff(componente_v, componente_u, w_max_x_comp, t_v, t_u)){
+            U->unite(componente_v, componente_u);
 
+            // Actualizo el peso mas grande de la componente de v
+            if(w(o_q) > w_max_x_comp[componente_v])
+                w_max_x_comp[componente_v] = w(o_q);
+        }
     }
 
     return T;
 }
 
-vector<eje> segmentar(const matriz& m){
-    unsigned int n = m.size() * m[0].size();
+vector<eje> segmentar(const matriz& m, int k, int metodo){
     int alto = m.size();
     int ancho = m[0].size();
+    unsigned int n = m.size() * m[0].size();
 
     // Armamos lista de aristas
     vector<eje> E;
@@ -95,14 +104,16 @@ vector<eje> segmentar(const matriz& m){
     /* cout<<endl; */
 
     // Creamos el disjoint_set aca para que el algoritmo sea igual sin importar la implementacion
+    /* shared_ptr<disjoint_set> U; */
     shared_ptr<disjoint_set> U;
-    /* shared_ptr<disjoint_set> U(new disjoint_set_arreglo()); */
+    if(metodo == 0)
+        U.reset(new disjoint_set_arreglo());
     //shared_ptr<disjoint_set> U = new disjoint_set_arbol();
     //shared_ptr<disjoint_set> U = new disjoint_set_arbol_optimizado();
 
     U->create(n);// alto * ancho
 
-    return segmentar_aux(E, U);
+    return segmentar_aux(E, U, n, k);
 }
 
 int main(int argc, char** argv){
@@ -131,7 +142,7 @@ int main(int argc, char** argv){
     mostrar_matriz(m);
 
     // Terminamos de leer
-    /* segmentar(m); */
+    /* segmentar(m, k, metodo); */
 
         
     return 0;
