56122efee07c07b85e6bd9a7d89cf874060c09bb
chino117 <chino117@hotmail.com>
2019-05-25 20:36:31 -0300

    Se sube dijsktra con arreglos, falta meter logica de lo q subio emi, se subio una prueba tambien del algoritmo

--- Resolucion/codigo/ej2/dijkstra.hpp
+++ Resolucion/codigo/ej2/dijkstra.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <queue>
 #include <tuple>
@@ -11,7 +12,7 @@ using namespace std;
 struct label_t{
    int nro;     /* numero del nodo */
    int prev;    /* nodo precedente (-1 para el nodo inicial )*/
-   int peso;    /* peso o coste total de la trayectoria que conduce al nodo, i.e., 
+   int coste;    /* coste o coste total de la trayectoria que conduce al nodo, i.e., 
                 el coste total desde el nodo inicial hasta el actual. Un valor de -1 
                 denota el infinito */
    int marca;   /* si el nodo ha sido marcado o no */
@@ -21,14 +22,14 @@ struct label_t{
 /* Calcula el coste minimo de alcanzar un nodo final 'b'
 * grafo no dirigido con N nodos, a partir del nodo inicial 'a',
 * dada su matriz de adyacencia A */
-void dijkstraAux(int N, const Matriz &A, int a, int b){
-	int i, i0, j, peso;
+void dijkstraAuxEjemplo(int N, const Matriz &A, int a, int b){
+	int i, i0, j, coste;
 
 	/* Crea el arreglo de etiquetas de nodo */
-	vector<label_t> Labels(N, -1);
+	vector<label_t> Labels(N);
 	/* nodo inicial 'a' entre 0 y N - 1 */
 	if ( a < 0 || a > N - 1 ) return;
-	/* nodo final 'a' entre 0 y N - 1 */
+	/* nodo final 'b' entre 0 y N - 1 */
 	if ( b < 0 || b > N - 1 ) return;
 
 	/* inicializar las etiquetas de nodo */
@@ -36,29 +37,30 @@ void dijkstraAux(int N, const Matriz &A, int a, int b){
 		Labels[i].nro = i;
 		if ( i != a ) {
 			Labels[i].prev = -1;   /* aun no se ha definido predecesor */
-			Labels[i].peso = -1;   /* infinito */
+			Labels[i].coste = infty;   /* infinito */
 			Labels[i].marca = 0;
-	} else {
+		} else {
 			Labels[i].prev = -1;   /* aun no se ha definido predecesor */
-			Labels[i].peso = 0;    /* coste del nodo inicial a si mismo es cero */
+			Labels[i].coste = 0;    /* coste del nodo inicial a si mismo es cero */
 			Labels[i].marca = 0;
 		}
 	}
 
 	/* continuamos este ciclo mientras existan nodos no marcados */
 	while ( 1 ) {
-		/* busca entre todos los nodos no marcados el de menor peso, descartando los
-		* de peso infinito (-1) */
-		peso = -1;
+		/* busca entre todos los nodos no marcados el de menor coste, descartando los
+		* de coste infinito (-1) */
+		coste = infty;
 		i0 = -1;
 		for ( i = 0; i < N; i++ ) {
-			if ( Labels[i].marca == 0 && Labels[i].peso >= 0 )
-			if ( peso == -1 ) {
-				peso = Labels[i].peso;
-				i0 = i;
-			} else if ( Labels[i].peso <= peso ) {
-				peso = Labels[i].peso;
-				i0 = i;
+			if ( Labels[i].marca == 0 && Labels[i].coste != infty ) {
+				if ( coste == infty ) {
+					coste = Labels[i].coste;
+					i0 = i;
+				} else if ( Labels[i].coste <= coste ) {
+					coste = Labels[i].coste;
+					i0 = i;
+				}
 			}
 		}
 		if ( i0 == -1 ) { /* termina si no encuentra */
@@ -68,19 +70,19 @@ void dijkstraAux(int N, const Matriz &A, int a, int b){
 
 		cout << "*** Analizando nodo " << i0 << " ***" << endl;
 
-		/* actualiza el peso de todos los sucesores (si los hay) del nodo
+		/* actualiza el coste de todos los sucesores (si los hay) del nodo
 		* encontrado y luego senala dicho nodo como marcado */
 		for ( i = 0; i < N; i++ ) {
 			if ( A[i0][i] > 0 ) {
 				/* si el coste acumulado sumado al coste del enlace del nodo i0 al nodo i
 				* es menor al coste del nodo i (o si el coste del nodo i es infinito),
 				* debemos actualizar */
-				if ( Labels[i].peso == -1 || Labels[i0].peso + A[i0][i] < Labels[i].peso ) {
-					if ( Labels[i0].peso + A[i0][i] < Labels[i].peso )
+				if ( Labels[i].coste == infty || Labels[i0].coste + A[i0][i] < Labels[i].coste ) {
+					if ( Labels[i0].coste + A[i0][i] < Labels[i].coste )
 						cout << "   [ mejorando coste de nodo " << i << " ]" << endl;
-					Labels[i].peso = Labels[i0].peso + A[i0][i];
+					Labels[i].coste = Labels[i0].coste + A[i0][i];
 					Labels[i].prev = i0;
-					cout << "   coste de nodo " << i << " desde nodo " << i0 << ": " << Labels[i].peso << endl;
+					cout << "   coste de nodo " << i << " desde nodo " << i0 << ": " << Labels[i].coste << endl;
 				}
 			}
 		}
@@ -90,8 +92,8 @@ void dijkstraAux(int N, const Matriz &A, int a, int b){
 		/* para verificar, imprime los costes calculados hasta el momento */
 		for ( i = 0; i < N; i++ ) {
 			cout << i << ": [";
-			if ( Labels[i].peso == -1 ) cout << "Inf";
-			else cout << Labels[i].peso;
+			if ( Labels[i].coste == infty ) cout << "Inf";
+			else cout << Labels[i].coste;
 			cout << ", " << Labels[i].prev ;
 			if ( Labels[i].marca == 1 ) cout <<  ", x]" << endl;
 			else cout << "]" << endl;
@@ -119,13 +121,94 @@ void dijkstraAux(int N, const Matriz &A, int a, int b){
 		cout << ruta[i];
 		if ( i < longitud - 1 ) cout << " - ";
 	}
-	cout << "Costo total: " << Labels[b].peso << endl;
+	cout << " Costo total: " << Labels[b].coste << endl;
 }
 
-Matriz dijkstra(int n, int m, const vector<int> &costoXciudad, const Matriz &litrosXeje) {
-	Matriz res;
-	resizeMatriz(res, n, n-1);
-	// res = dijkstraAux(n, costoXciudad, litrosXeje);
+// falta aplicar logica del problema al algoritmo
+MatrizRes dijkstra(int n, const Matriz &A){
+	int i, i0, j, coste;
+	MatrizRes res;
+	/* Crea el arreglo de etiquetas de nodo */
+	vector<label_t> Labels(n);
+	int a = 0;
+
+	/* inicializar las etiquetas de nodo */
+	for ( i = 0; i < n; i++ ) {
+		Labels[i].nro = i;
+		if ( i != a ) {
+			Labels[i].prev = -1;   /* aun no se ha definido predecesor */
+			Labels[i].coste = infty;   /* infinito */
+			Labels[i].marca = 0;
+		} else {
+			Labels[i].prev = -1;   /* aun no se ha definido predecesor */
+			Labels[i].coste = 0;    /* coste del nodo inicial a si mismo es cero */
+			Labels[i].marca = 0;
+		}
+	}
+
+	/* continuamos este ciclo mientras existan nodos no marcados */
+	while ( 1 ) {
+		/* busca entre todos los nodos no marcados el de menor coste, descartando los
+		* de coste infinito (-1) */
+		coste = infty;
+		i0 = -1;
+		for ( i = 0; i < n; i++ ) {
+			if ( Labels[i].marca == 0 && Labels[i].coste != infty ) {
+				if ( coste == infty ) {
+					coste = Labels[i].coste;
+					i0 = i;
+				} else if ( Labels[i].coste <= coste ) {
+					coste = Labels[i].coste;
+					i0 = i;
+				}
+			}
+		}
+		if ( i0 == -1 ) { /* termina si no encuentra */
+			break;
+		}
+		/* actualiza el coste de todos los sucesores (si los hay) del nodo
+		* encontrado y luego senala dicho nodo como marcado */
+		for ( i = 0; i < n; i++ ) {
+			if ( A[i0][i] > 0 ) {
+				/* si el coste acumulado sumado al coste del enlace del nodo i0 al nodo i
+				* es menor al coste del nodo i (o si el coste del nodo i es infinito),
+				* debemos actualizar */
+				if ( Labels[i].coste == infty || Labels[i0].coste + A[i0][i] < Labels[i].coste ) {
+					Labels[i].coste = Labels[i0].coste + A[i0][i];
+					Labels[i].prev = i0;
+				}
+			}
+		}
+		Labels[i0].marca = 1;
+	}
+	// ARMAR TODAS LAS RUTAS, no tegno en cuenta nodos con costes negativos
+	int si;
+	tuple<int,int,int> tripla;
+	resizeMatrizRes(res, n, n-1);
+	for (i = 0; i < n; ++i) {
+		j = (i + 1) % n;
+		while (j != i) {
+			/* Ruta desde el nodo 'i' hasta el nodo 'j' */			
+			si = abs(Labels[i].coste - Labels[j].coste);
+
+			get<0>(tripla) = i;
+			get<1>(tripla) = j;
+			get<2>(tripla) = si;
+			res[i][j] = tripla;
+
+			get<0>(tripla) = j;
+			get<1>(tripla) = i;
+			get<2>(tripla) = si;
+			res[j][i] = tripla;
+
+			j++;
+		}
+	}
 }
+
+// vector<vector<int>> dijkstra(int n, int m, const vector<int> &costoXciudad, const Matriz &litrosXeje) {
+	
+// 	return res;
+// }
 
 
